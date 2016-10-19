8fad87b6e92c1f04ca753d638f0708623a40d9e2
Phil Fenstermacher <phillip.fenstermacher@gmail.com>
2016-10-19 13:15:44 -0700

    The latest version of filebeat should be installed on windows by default

--- manifests/params.pp
+++ manifests/params.pp
@@ -43,7 +43,7 @@ class filebeat::params {
     'Windows' : {
       $config_file      = 'C:/Program Files/Filebeat/filebeat.yml'
       $config_dir       = 'C:/Program Files/Filebeat/conf.d'
-      $download_url     = 'https://download.elastic.co/beats/filebeat/filebeat-1.2.3-windows.zip'
+      $download_url     = 'https://download.elastic.co/beats/filebeat/filebeat-1.3.1-windows.zip'
       $install_dir      = 'C:/Program Files'
       $tmp_dir          = 'C:/Windows/Temp'
       $service_provider = undef
