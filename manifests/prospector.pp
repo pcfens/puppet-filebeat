e4bedcb03020743bea68efa3279dc58d7b9766b4
olofhellqvist <olof.hellqvist@gmail.com>
2016-11-30 13:56:13 +0100

    Update prospector.pp

--- manifests/prospector.pp
+++ manifests/prospector.pp
@@ -31,6 +31,17 @@ define filebeat::prospector (
   }
 
   case $::kernel {
+    'FreeBSD' : {
+      file { "filebeat-${name}":
+        ensure  => $ensure,
+        path    => "${filebeat::config_dir}/${name}.yml",
+        owner   => 'root',
+        group   => 'wheel',
+        mode    => $::filebeat::config_file_mode,
+        content => template("${module_name}/${prospector_template}"),
+        notify  => Service['filebeat'],
+      }
+    }
     'Linux' : {
       file { "filebeat-${name}":
         ensure  => $ensure,
