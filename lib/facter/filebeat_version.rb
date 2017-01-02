1b6b4ef2c7a25a11830585277fea44e8be9d171b
Phil Fenstermacher <phillip.fenstermacher@gmail.com>
2017-01-02 13:16:11 -0500

    Fix the filebeat_version fact in windows
    Fixes #59

--- lib/facter/filebeat_version.rb
+++ lib/facter/filebeat_version.rb
@@ -1,14 +1,14 @@
 require 'facter'
 Facter.add('filebeat_version') do
   confine :kernel => %w(Linux Windows)
+  if File.executable?('/usr/bin/filebeat')
+    filebeat_version = Facter::Util::Resolution.exec('/usr/bin/filebeat --version')
+  elsif File.executable?('/usr/share/filebeat/bin/filebeat')
+    filebeat_version = Facter::Util::Resolution.exec('/usr/share/filebeat/bin/filebeat --version')
+  elsif File.exists?('c:\Program Files\Filebeat\filebeat.exe')
+    filebeat_version = Facter::Util::Resolution.exec('"c:\Program Files\Filebeat\filebeat.exe" --version')
+  end
   setcode do
-    if File.executable?('/usr/bin/filebeat')
-      filebeat_version = Facter::Util::Resolution.exec('/usr/bin/filebeat --version')
-    elsif File.executable?('/usr/share/filebeat/bin/filebeat')
-      filebeat_version = Facter::Util::Resolution.exec('/usr/share/filebeat/bin/filebeat --version')
-    elsif File.executable?('C:/Program Files/Filebeat/filebeat.exe --version')
-      filebeat_version = Facter::Util::Resolution.exec('C:/Program Files/Filebeat/filebeat.exe --version')
-    end
-    %r{^filebeat version ([^\s]+)?}.match(filebeat_version)[1] unless filebeat_version.nil?
+    %r{^filebeat version ([^\s]+)?}.match(filebeat_version)[1]
   end
 end
