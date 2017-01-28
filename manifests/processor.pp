23db2d1392f0195bd836191d0fd9f1e0297fc848
corey-hammerton <corey.hammerton@gmail.com>
2017-01-28 15:58:22 -0500

    Reworking the processor implementation.
    Instead of, what can become, a complicated original implementation migrating
    filebeat processors to it's own defined type. There it's much easier to perform
    some value checking before generating the configuration files in the config
    dir location.

---
+++ manifests/processor.pp
@@ -0,0 +1,74 @@
+define filebeat::processor(
+  $ensure         = present,
+  $priority       = 10,
+  $processor_name = $name,
+  $params         = undef,
+  $when           = undef,
+) {
+  include ::filebeat
+
+  validate_integer($priority)
+  validate_string($processor_name)
+
+  if versioncmp("${filebeat::real_version}", '5') < 0 {
+    fail("Processors only work on Filebeat 5.0 and higher")
+  }
+
+  if $priority < 10 {
+    $_priority = "0${priority}"
+  }
+  else {
+    $_priority = "${priority}"
+  }
+
+  if $processor_name == "drop_field" and $when == undef {
+    fail("drop_event processors require a condition, without one ALL events are dropped")
+  }
+  elsif $processor_name != 'add_cloud_metadata' and $params == undef {
+    fail("${processor_name} requires parameters to function as expected")
+  }
+
+  if $processor_name == 'add_cloud_metadata' {
+    $_configuration = delete_undef_values(merge({"timeout" => "3s"}, $params))
+  }
+  elsif $processor_name == 'drop_field' {
+    $_configuration = $when
+  }
+  else {
+    $_configuration = delete_undef_values(merge({"when" => $when}, $params))
+  }
+
+  $filename         = "${filebeat::config_dir}/${_priority}-processor-${name}.yml"
+  $processor_config = delete_undef_values({
+    "processors" => [
+      {
+        $processor_name => $_configuration,
+      }
+    ]
+  })
+
+  case $::kernel {
+    'Linux': {
+      file{"${filename}":
+        ensure       => $ensure,
+        owner        => 'root',
+        group        => 'root',
+        mode         => $::filebeat::config_file_mode,
+        content      => inline_template("<%= @processor_config.to_yaml() %>"),
+        validate_cmd => "/usr/share/filebeat/bin/filebeat -N -configtest -c %",
+        notify       => Class["filebeat::service"],
+      }
+    }
+    'Windows': {
+      file{"${filename}":
+        ensure       => $ensure,
+        content      => inline_template("<%= @processor_config.to_yaml() %>"),
+        validate_cmd => "c:\\Program Files\\Filebeat\\filebeat.exe -N -configtest -c %",
+        notify       => Class["filebeat::service"],
+      }
+    }
+    default: {
+      fail($filebeat::kernel_fail_message)
+    }
+  }
+}
---
+++ spec/defines/processor_spec.rb
@@ -0,0 +1,116 @@
+require 'spec_helper'
+
+describe 'filebeat::processor', type: :define do
+  let :pre_condition do
+    'class{"filebeat":
+      outputs => {
+        "logstash" => {
+          "hosts" => [
+            "localhost:5044",
+          ],
+        },
+      },
+    }'
+  end
+  let :title do
+    'test-processor'
+  end
+
+  context 'with no parameters' do
+    it { is_expected.to raise_error(Puppet::Error) }
+  end
+
+  context 'on Linux' do
+    let :facts do
+      {
+        kernel: 'Linux',
+        osfamily: 'Linux',
+        rubyversion: '2.3.1',
+        filebeat_version: '5.1.2'
+      }
+    end
+
+    context 'add_cloud_metadata processor' do
+      let :params do
+        {
+          processor_name: 'add_cloud_metadata',
+        }
+      end
+
+      it do
+        is_expected.to contain_file('/etc/filebeat/conf.d/10-processor-test-processor.yml').with(
+          mode: '0644',
+          content: '---
+processors:
+- add_cloud_metadata:
+    timeout: 3s
+'
+        )
+      end
+    end
+
+    context 'drop_event processor with no conditions' do
+      let :params do
+        {
+          processor_name: 'drop_event'
+        }
+      end
+
+      it { is_expected.to raise_error(Puppet::Error) }
+    end
+
+    context 'drop_event processor with conditions' do
+      let :params do
+        {
+          processor_name: 'drop_event',
+          when: {regex: {message: '^DEBUG'}}
+        }
+      end
+
+      it do
+        is_expected.to contain_file('/etc/filebeat/conf.d/10-processor-test-processor.yml').with(
+          mode: '0644',
+          content: '---
+processors:
+- drop_event:
+    when:
+      regex:
+        message: ^DEBUG
+'
+        )
+      end
+    end
+
+    context 'drop_field processor with no params' do
+      let :params do
+        {
+          processor_name: 'drop_field'
+        }
+      end
+
+      it { is_expected.to raise_error(Puppet::Error) }
+    end
+
+    context 'drop_field processor with fields' do
+      let :params do
+        {
+          processor_name: 'drop_field',
+          params: {fields: ['input_type', 'offset']}
+        }
+      end
+
+      it do
+        is_expected.to contain_file('/etc/filebeat/conf.d/10-processor-test-processor.yml').with(
+          mode: '0644',
+          content: '---
+processors:
+- drop_field:
+    fields:
+      - input_type
+      - offset
+'
+        )
+      end
+    end
+  end
+end
