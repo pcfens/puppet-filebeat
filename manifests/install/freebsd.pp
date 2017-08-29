3d564c98d9c8007b75db32b9b471b8b1d732d149
Alessandro Lorenzi <alessandro@lm-net.it>
2017-08-29 15:18:28 +0200

    FreeBSD: overrite default rc.d filebeat

--- manifests/install/freebsd.pp
+++ manifests/install/freebsd.pp
@@ -1,3 +1,10 @@
 class filebeat::install::freebsd {
   ensure_packages (['beats'], {ensure => $filebeat::package_ensure})
+  file {'/usr/local/etc/rc.d/filebeat':
+    ensure => present,
+    owner  => 'root',
+    group  => 'wheel',
+    mode   => '0555',
+    source => 'puppet:///modules/filebeat/FreeBSD/rc.d_filebeat',
+  }
 }
