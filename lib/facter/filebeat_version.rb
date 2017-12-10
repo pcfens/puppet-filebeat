1eb787a9f828439eadb87c4ee74dc1c1d42df36e
Luca Lesinigo <luca@lm-net.it>
2017-12-10 17:38:49 +0100

    add FreeBSD support to filebeat_version custom fact

--- lib/facter/filebeat_version.rb
+++ lib/facter/filebeat_version.rb
@@ -1,9 +1,11 @@
 require 'facter'
 Facter.add('filebeat_version') do
-  confine 'kernel' => %w[Linux Windows]
+  confine 'kernel' => %w[FreeBSD Linux Windows]
   if File.executable?('/usr/bin/filebeat')
     filebeat_version = Facter::Util::Resolution.exec('/usr/bin/filebeat --version')
   elsif File.executable?('/usr/share/filebeat/bin/filebeat')
+    filebeat_version = Facter::Util::Resolution.exec('/usr/share/filebeat/bin/filebeat --version')
+  elsif File.executable?('/usr/local/sbin/filebeat')
     filebeat_version = Facter::Util::Resolution.exec('/usr/share/filebeat/bin/filebeat --version')
   elsif File.exist?('c:\Program Files\Filebeat\filebeat.exe')
     filebeat_version = Facter::Util::Resolution.exec('"c:\Program Files\Filebeat\filebeat.exe" --version')
