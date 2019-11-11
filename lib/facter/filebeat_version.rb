f2fb7ec250588bfc02f9afe6277a63e4cf3aa4ad
Chris Heerschap <cheerschap@us.ibm.com>
2019-11-11 14:35:23 -0500

    Update facter script to work with hideously old filebeat

--- lib/facter/filebeat_version.rb
+++ lib/facter/filebeat_version.rb
@@ -3,6 +3,9 @@ Facter.add('filebeat_version') do
   confine 'kernel' => ['FreeBSD', 'OpenBSD', 'Linux', 'Windows']
   if File.executable?('/usr/bin/filebeat')
     filebeat_version = Facter::Util::Resolution.exec('/usr/bin/filebeat version')
+    if filebeat_version.empty?
+      filebeat_version = Facter::Util::Resolution.exec('/usr/bin/filebeat --version')
+    end
   elsif File.executable?('/usr/local/bin/filebeat')
     filebeat_version = Facter::Util::Resolution.exec('/usr/local/bin/filebeat --version')
   elsif File.executable?('/usr/share/filebeat/bin/filebeat')
