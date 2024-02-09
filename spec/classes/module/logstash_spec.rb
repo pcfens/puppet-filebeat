b6e477356892f1c9dbf4874a8cb5532e963fc5bc
Stephan Eicher <stephan.eicher@check24.de>
2024-02-09 23:11:15 +0100

    Add logstash module

---
+++ spec/classes/module/logstash_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'filebeat::module::logstash' do
+  let :pre_condition do
+    'include ::filebeat'
+  end
+
+  let(:facts) { 
+    {
+      :kernel => 'Linux',
+      :os => {
+        :family => 'Debian',
+        :name => 'Ubuntu',
+      }
+    } 
+  }
+  
+  context 'on default values' do
+    it { is_expected.to compile.with_all_deps }
+
+    it {
+      is_expected.to contain_file('filebeat-module-logstash').with_content(
+      %r{- module: logstash\n\s{2}log:\n\s{4}enabled: false\n\s{2}slowlog:\n\s{4}enabled: false\n\n},
+    )}
+  end
+
+  context 'on log and slowlog enabled with paths' do
+    let(:params) do
+      {
+        'log_enabled' => true,
+        'log_paths' => ['/var/log/logstash.log'],
+        'slowlog_enabled' => true,
+        'slowlog_paths' => ['/var/log/logstash-slowlog.log'],
+      }
+    end
+
+    it { is_expected.to compile.with_all_deps }
+
+    it {
+      is_expected.to contain_file('filebeat-module-logstash').with_content(
+        <<-EOS
+### Filebeat configuration managed by Puppet ###
+---
+- module: logstash
+  log:
+    enabled: true
+    var.paths:
+    - "/var/log/logstash.log"
+  slowlog:
+    enabled: true
+    var.paths:
+    - "/var/log/logstash-slowlog.log"
+
+EOS
+    )
+    }
+  end
+end
